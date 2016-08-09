1d8a266f994ed7c3b537fe733be6662862a1d7d9
dunarobert <dunarobert7@gmail.com>
2016-08-09 11:10:56 +0300

    Update 1

---
+++ AnonymusMethod/AnonymusMethod/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonymusMethod
+{
+    class Program
+    {
+        public static bool IsUnderThirty(Employee e)
+        {
+            return e.Age < 30;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Employee> listofEmployees = new List<Employee>(){
+               new Employee(){Name = "Alin",Age=20,Occupation = "Engineer"},
+               new Employee(){Name = "Alexandru",Age = 34,Occupation = "Programmer"},
+               new Employee(){Name = "Ion",Age=29,Occupation = "Tester"},
+               new Employee(){Name = "Iancu",Age=37,Occupation = "Tester"},
+               new Employee(){Name = "Marian",Age=25,Occupation = "WebDeveloper"},
+           };
+
+            Predicate<Employee> callback = IsUnderThirty;
+            List<Employee> employeeUnderThirty = listofEmployees.FindAll(callback);
+            foreach (Employee e in employeeUnderThirty)
+            {
+                Console.WriteLine("{0} are sub 30 de ani", e.Name);
+            }
+
+            // Selectare persoane care au job-ul tester
+
+            List<Employee> testerEmployees = listofEmployees.FindAll(delegate(Employee e) { return e.Occupation == "Tester"; });
+
+            foreach(Employee emp in testerEmployees){
+                Console.WriteLine("Tester : {0}",emp.Name);
+            }
+        }
+    }
+}
